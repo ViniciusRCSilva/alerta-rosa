c2b776e4fcca4497e2b42da957d95237eaea8778
Vinicius <viniciusflexa@gmail.com>
2023-09-29 16:58:29 -0300

    fix

--- src/app/(home)/components/Topbar.tsx
+++ src/app/(home)/components/Topbar.tsx
@@ -9,12 +9,7 @@ export function Topbar() {
     <div className="fixed w-full py-2 px-10 bg-[rgba(51,51,48,0.4)] shadow-md backdrop-blur-md">
       <div className="flex flex-row w-full items-center justify-between">
         <Link href="/">
-          <Image 
-            src={'mini_logo.svg'} 
-            alt="logo" 
-            height={50}
-            width={50}
-          />
+          <Image src={'mini_logo.svg'} alt="logo" height={50} width={50} />
         </Link>
 
         <div className="hidden lg:flex flex-row text-white gap-5">
