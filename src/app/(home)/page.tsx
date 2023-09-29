fa5df97bcb3c89dba55f04219e72db7d19a16d7c
Davi Souza <davisouza2001dv@gmail.com>
2023-09-29 10:24:22 -0300

    fixed eslint

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
     /* Conteúdo geral */
     <main>
       {/* Conteúdo Principal */}
-      <div className="w-full h-screen bg-[url('../../public/main_content.svg')] bg-no-repeat bg-center bg-cover">
-        
-      </div>
-      <div className="w-full h-screen">
-        
-      </div>
+      <div className="w-full h-screen bg-[url('../../public/main_content.svg')] bg-no-repeat bg-center bg-cover"></div>
+      <div className="w-full h-screen"></div>
     </main>
   )
 }
