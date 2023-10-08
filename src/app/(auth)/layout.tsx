e4d4e2fff61a81d4ce7421ecf2e0eee602d71c0c
Davi Souza <davisouza2001dv@gmail.com>
2023-10-08 20:41:29 -0300

    remove html tag duplicated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,4 @@
 import Image from 'next/image'
-import { Lexend } from 'next/font/google'
-
-const lexend = Lexend({ subsets: ['latin'] })
 
 export default function AuthLayout({
   children,
@@ -9,64 +6,54 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={lexend.className}>
-        <main className="flex flex-row w-full h-screen items-center justify-between">
-          {/* Conteúdo da esquerda */}
-          <div
-            className="hidden lg:flex flex-col w-[50%] h-full items-center justify-center gap-10
+    <div className="flex flex-row w-full h-screen items-center justify-between">
+      <div
+        className="hidden lg:flex flex-col w-[50%] h-full items-center justify-center gap-10
           text-white
             bg-[url('../../public/left_side_background.svg')] bg-no-repeat bg-center bg-cover shadow-inner-shadow"
-          >
-            <div className="flex flex-col items-center text-5xl font-bold z-10 gap-2">
-              <p>Seja</p>
-              <p>Bem-Vinda!</p>
-            </div>
-
-            <Image
-              src={'heart_women.svg'}
-              alt="mulher segurando coração"
-              className="z-0"
-              height={200}
-              width={200}
-            />
-
-            <div className="flex flex-col items-center text-2xl font-light z-10">
-              <p>Aqui</p>
-              <p>Você importa.</p>
-              <p>Sua voz importa.</p>
-              <p>Sua vida importa.</p>
-            </div>
-          </div>
-
-          {/* Conteúdo da direita */}
-          <div
-            className="flex flex-col w-full lg:w-[50%] h-full items-center justify-center gap-5 lg:gap-10
+      >
+        <div className="flex flex-col items-center text-5xl font-bold z-10 gap-2">
+          <p>Seja</p>
+          <p>Bem-Vinda!</p>
+        </div>
+        <Image
+          src={'heart_women.svg'}
+          alt="mulher segurando coração"
+          className="z-0"
+          height={200}
+          width={200}
+        />
+        <div className="flex flex-col items-center text-2xl font-light z-10">
+          <p>Aqui</p>
+          <p>Você importa.</p>
+          <p>Sua voz importa.</p>
+          <p>Sua vida importa.</p>
+        </div>
+      </div>
+      <div
+        className="flex flex-col w-full lg:w-[50%] h-full items-center justify-center gap-5 lg:gap-10
             text-dark-blue 
             lg:bg-[url('../../public/right_side_background.svg')] 
             bg-[url('../../public/mobile_background_login.svg')] bg-no-repeat bg-center bg-cover"
-          >
-            {/* Logo */}
-            <Image
-              src={'logo.svg'}
-              alt="logo"
-              className="hidden lg:flex"
-              height={300}
-              width={300}
-            />
+      >
+        <Image
+          src={'logo.svg'}
+          alt="logo"
+          className="hidden lg:flex"
+          height={300}
+          width={300}
+        />
 
-            <Image
-              src={'logo.svg'}
-              alt="logo"
-              className="flex lg:hidden"
-              height={200}
-              width={200}
-            />
+        <Image
+          src={'logo.svg'}
+          alt="logo"
+          className="flex lg:hidden"
+          height={200}
+          width={200}
+        />
 
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
+        {children}
+      </div>
+    </div>
   )
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,10 +7,7 @@ import { Icon } from '@chakra-ui/react'
 export default function Login() {
   return (
     <div className="flex flex-col items-center justify-center gap-5 lg:gap-10 animate-screenOpacity">
-      {/* Título */}
       <p className="font-bold text-3xl">LOGIN</p>
-
-      {/* Formulário */}
       <div className="flex flex-col gap-5">
         <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
           <Icon
@@ -43,13 +40,10 @@ export default function Login() {
           />
         </div>
       </div>
-
-      {/* Botão entrar / Link para criar conta */}
       <div className="flex flex-col lg:flex-row lg:items-center gap-10">
         <button className="bg-pink text-white p-2 px-5 rounded-full">
           Entrar
         </button>
-
         <div className="flex flex-col items-center lg:items-end">
           <p className="font-light">Não tem uma conta?</p>
           <Link href="/cadastro" className="text-pink underline">
@@ -57,21 +51,15 @@ export default function Login() {
           </Link>
         </div>
       </div>
-
-      {/* Separador */}
       <div className="flex flex-row items-center gap-4">
         <div className="h-1 px-14 bg-pink rounded-full" />
         <p>ou</p>
         <div className="h-1 px-14 bg-pink rounded-full" />
       </div>
-
-      {/* Botão entrar com Google */}
       <button className="flex flex-row items-center gap-4 p-2 px-5 border-2 border-dark-blue bg-white rounded-full">
         <Image src={'google_icon.svg'} alt="google" height={25} width={25} />
         Entrar com Google
       </button>
-
-      {/* Fim da página */}
     </div>
   )
 }
