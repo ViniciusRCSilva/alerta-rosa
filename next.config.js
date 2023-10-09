218c5df0ce65687fb78e963c0a79a89cd04597eb
Vinicius <viniciusflexa@gmail.com>
2023-10-09 12:12:17 -0300

    image link

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ module.exports = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
