b4ec40a1e648727c23decc60d3250b655aadb4f7
Vinicius <viniciusflexa@gmail.com>
2023-10-18 16:59:11 -0300

    update

--- src/app/(home)/components/ButtonInfoViolentometro.tsx
+++ src/app/(home)/components/ButtonInfoViolentometro.tsx
@@ -4,11 +4,9 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
   useDisclosure,
-  Button,
 } from '@chakra-ui/react'
 
 interface ButtonInfoProps {
