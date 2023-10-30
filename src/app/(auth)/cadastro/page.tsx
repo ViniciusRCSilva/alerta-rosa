d220d5183ff133c920cc1f30b91cc0fb20099b1a
Vinicius <viniciusflexa@gmail.com>
2023-10-30 17:18:32 -0300

    updates cadastro

--- src/app/(auth)/cadastro/page.tsx
+++ src/app/(auth)/cadastro/page.tsx
@@ -1,11 +1,20 @@
 'use client'
-import { EnvelopeSimple, Password } from '@phosphor-icons/react'
+import {
+  EnvelopeSimple,
+  MapPin,
+  MapTrifold,
+  Password,
+  Phone,
+  UserCircle,
+} from '@phosphor-icons/react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { z } from 'zod'
+import { number, z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import UseAuth from '@/service/hooks/useAuth'
+import { EstadoCidadeSelects } from '../components/EstadoCidadeSelect'
+import { useState } from 'react'
 
 const createRegisterFormSchema = z.object({
   name: z.string().nonempty('O nome é obrigatório!'),
@@ -26,6 +35,12 @@ const createRegisterFormSchema = z.object({
 type CreateRegisterFormData = z.infer<typeof createRegisterFormSchema>
 
 export default function Register() {
+  const [contentView, setContentView] = useState<'part1' | 'part2'>('part1')
+
+  function handleChangeView(view: 'part1' | 'part2') {
+    setContentView(view)
+  }
+
   const { submitUser, user, createUserPassword } = UseAuth()
   const { push } = useRouter()
   const {
@@ -47,111 +62,148 @@ export default function Register() {
   return (
     <div className="flex flex-col items-center justify-center gap-5 lg:gap-10 animate-screenOpacity">
       <p className="font-bold text-3xl">CADASTRO</p>
-      <form
-        onSubmit={handleSubmit(handleRegisterSubmit)}
-        className="flex flex-col gap-5"
-      >
-        <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
-          <EnvelopeSimple className="text-2xl text-pink" />
-          {user?.name ? (
-            <input
-              className="px-2 focus:outline-none"
-              type="text"
-              placeholder="Name"
-              required
-              value={user.name}
-              readOnly
-              {...register('name')}
-            />
-          ) : (
-            <input
-              className="px-2 focus:outline-none"
-              type="text"
-              placeholder="Name"
-              required
-              {...register('name')}
-            />
+      <form onSubmit={handleSubmit(handleRegisterSubmit)}>
+        <>
+          {contentView === 'part1' && (
+            <>
+              <div className="flex items-center justify-center mb-5 gap-2">
+                <div className="flex w-10 h-10 items-center justify-center rounded-full p-2 text-lg bg-pink text-white select-none">
+                  <p>1</p>
+                </div>
+                <div className="flex w-2 h-2 rounded-full p-2 text-lg bg-dark-blue opacity-80"></div>
+                <div className="flex w-2 h-2 rounded-full p-2 text-lg bg-dark-blue opacity-70"></div>
+                <div className="flex w-2 h-2 rounded-full p-2 text-lg bg-dark-blue opacity-60"></div>
+                <div className="flex w-10 h-10 items-center justify-center rounded-full p-2 text-lg bg-pink text-white opacity-50 select-none">
+                  <p>2</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-5 animate-screenOpacity">
+                <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
+                  <UserCircle className="text-2xl text-pink" />
+                  {user?.name ? (
+                    <input
+                      className="px-2 focus:outline-none"
+                      type="text"
+                      placeholder="Nome"
+                      required
+                      value={user.name}
+                      readOnly
+                      {...register('name')}
+                    />
+                  ) : (
+                    <input
+                      className="px-2 focus:outline-none"
+                      type="text"
+                      placeholder="Nome"
+                      required
+                      {...register('name')}
+                    />
+                  )}
+                </div>
+                {errors.name && <span>{errors.name.message}</span>}
+                <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
+                  <EnvelopeSimple className="text-2xl text-pink" />
+                  {user?.email ? (
+                    <input
+                      className="px-2 focus:outline-none"
+                      type="email"
+                      placeholder="Email"
+                      required
+                      value={user.email}
+                      readOnly
+                      {...register('email')}
+                    />
+                  ) : (
+                    <input
+                      className="px-2 focus:outline-none"
+                      type="email"
+                      placeholder="Email"
+                      required
+                      {...register('email')}
+                    />
+                  )}
+                </div>
+                {errors.email && <span>{errors.email.message}</span>}
+                <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
+                  <Password className="text-2xl text-pink" />
+                  <input
+                    className="px-2 focus:outline-none"
+                    type="password"
+                    placeholder="Senha"
+                    required
+                    {...register('password')}
+                  />
+                </div>
+                {errors.password && <span>{errors.password.message}</span>}
+                <div className="flex flex-row items-center justify-between">
+                  <Link
+                    href="/login"
+                    className="text-dark-blue font-light underline"
+                  >
+                    Cancelar
+                  </Link>
+                  <button
+                    onClick={() => handleChangeView('part2')}
+                    className="bg-pink text-white p-2 px-5 rounded-full"
+                  >
+                    Avançar
+                  </button>
+                </div>
+              </div>
+            </>
           )}
-        </div>
-        {errors.name && <span>{errors.name.message}</span>}
-        <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
-          <EnvelopeSimple className="text-2xl text-pink" />
-          {user?.email ? (
-            <input
-              className="px-2 focus:outline-none"
-              type="email"
-              placeholder="Email"
-              required
-              value={user.email}
-              readOnly
-              {...register('email')}
-            />
-          ) : (
-            <input
-              className="px-2 focus:outline-none"
-              type="email"
-              placeholder="Email"
-              required
-              {...register('email')}
-            />
+          {contentView === 'part2' && (
+            <>
+              <div className="flex items-center justify-center mb-5 gap-2">
+                <div className="flex w-10 h-10 items-center justify-center rounded-full p-2 text-lg bg-pink opacity-50 text-white select-none">
+                  <p>1</p>
+                </div>
+                <div className="flex w-2 h-2 rounded-full p-2 text-lg bg-dark-blue opacity-60"></div>
+                <div className="flex w-2 h-2 rounded-full p-2 text-lg bg-dark-blue opacity-70"></div>
+                <div className="flex w-2 h-2 rounded-full p-2 text-lg bg-dark-blue opacity-80"></div>
+                <div className="flex w-10 h-10 items-center justify-center rounded-full p-2 text-lg bg-pink text-white select-none">
+                  <p>2</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-5 animate-screenOpacity">
+                <EstadoCidadeSelects
+                  city={register('city')}
+                  state={register('state')}
+                />
+                {errors.state && <span>{errors.state.message}</span>}
+                {errors.city && <span>{errors.city.message}</span>}
+                <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
+                  <Phone className="text-2xl text-pink" />
+                  <input
+                    className="px-2 focus:outline-none"
+                    type="number"
+                    placeholder="Contato"
+                    maxLength={11}
+                    required
+                  />
+                </div>
+                {errors.phone && <span>{errors.phone.message}</span>}
+
+                <div className="flex flex-row items-center justify-between">
+                  <p
+                    onClick={() => handleChangeView('part1')}
+                    className="text-dark-blue font-light underline cursor-pointer"
+                  >
+                    Voltar
+                  </p>
+                  <button
+                    type="submit"
+                    className="bg-pink text-white p-2 px-5 rounded-full"
+                  >
+                    Concluir
+                  </button>
+                </div>
+              </div>
+            </>
           )}
-        </div>
-        {errors.email && <span>{errors.email.message}</span>}
-        <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
-          <EnvelopeSimple className="text-2xl text-pink" />
-          <input
-            className="px-2 focus:outline-none"
-            type="text"
-            placeholder="Estado"
-            required
-            {...register('state')}
-          />
-        </div>
-        {errors.state && <span>{errors.state.message}</span>}
-        <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
-          <EnvelopeSimple className="text-2xl text-pink" />
-          <input
-            className="px-2 focus:outline-none"
-            type="text"
-            placeholder="Cidade"
-            required
-            {...register('city')}
-          />
-        </div>
-        {errors.city && <span>{errors.city.message}</span>}
-        <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
-          <EnvelopeSimple className="text-2xl text-pink" />
-          <input
-            className="px-2 focus:outline-none"
-            type="text"
-            placeholder="Telefone"
-            required
-            {...register('phone')}
-          />
-        </div>
-        {errors.phone && <span>{errors.phone.message}</span>}
-        <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
-          <Password className="text-2xl text-pink" />
-          <input
-            className="px-2 focus:outline-none"
-            type="password"
-            placeholder="Senha"
-            required
-            {...register('password')}
-          />
-        </div>
-        {errors.password && <span>{errors.password.message}</span>}
-        <div className="flex flex-row items-center gap-10">
-          <Link href="/login" className="text-dark-blue font-light underline">
-            Cancelar
-          </Link>
-          <button
-            type="submit"
-            className="bg-pink text-white p-2 px-5 rounded-full"
-          >
-            Avançar
-          </button>
-        </div>
+        </>
       </form>
     </div>
   )
---
+++ src/app/(auth)/components/EstadoCidadeSelect.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { useEffect, useState } from 'react'
+import data from './data.json'
+import { MapPin, MapTrifold } from '@phosphor-icons/react'
+import { UseFormRegisterReturn } from 'react-hook-form'
+
+interface Estado {
+  sigla: string
+  nome: string
+  cidades: string[]
+}
+
+interface EstadoCidadeSelectsProps {
+  state: UseFormRegisterReturn<'state'>
+  city: UseFormRegisterReturn<'city'>
+}
+
+export function EstadoCidadeSelects(props: EstadoCidadeSelectsProps) {
+  const [estadoAtivo, setEstadoAtivo] = useState('AC')
+  const [cidadeAtiva, setCidadeAtiva] = useState<Estado | null>(null)
+  const estados = data.estados
+
+  console.log(estadoAtivo)
+
+  useEffect(() => {
+    const cidadeSelecionada = estados.find((cid) => estadoAtivo === cid.sigla)
+
+    cidadeSelecionada && setCidadeAtiva(cidadeSelecionada)
+  }, [estadoAtivo])
+
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
+        <MapTrifold className="text-2xl text-pink" />
+        <select className="px-2 focus:outline-none">
+          <option disabled>Selecione um estado</option>
+          {estados.map((estado) => (
+            <option key={estado.sigla} value={estado.sigla}>
+              {estado.nome}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-row items-center rounded-full border-2 p-2 px-5 border-pink bg-white">
+        <MapPin className="text-2xl text-pink" />
+        <select className="px-2 focus:outline-none" {...props.city}>
+          <option>Selecione uma cidade</option>
+          {cidadeAtiva?.cidades.map((cid, index) => {
+            return (
+              <option key={index} value={cid}>
+                {cid}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+    </div>
+  )
+}
