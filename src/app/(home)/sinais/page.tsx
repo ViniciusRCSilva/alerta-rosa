545acecf01b6794173bc77f08cf7e890e15860d7
Davi Souza <davisouza2001dv@gmail.com>
2023-11-28 17:56:03 -0300

    feat: added image on sinais

--- src/app/(home)/sinais/page.tsx
+++ src/app/(home)/sinais/page.tsx
@@ -58,6 +58,45 @@ export default function MedidasProtetivas() {
             </p>
           </div>
         </div>
+
+        <div
+          className="flex flex-col lg:flex-row w-full items-center justify-between py-20 lg:p-0
+        lg:bg-[url('../../public/right_side_background.svg')] 
+        bg-[url('../../public/mobile_background_login.svg')] bg-no-repeat bg-cover"
+        >
+          <div
+            className={`block lg:hidden w-40 h-40 rounded-lg
+        bg-[url('../../public/image_close_hand.png')]
+        lg:bg-[url('../../public/image_close_hand.png')] bg-no-repeat bg-center bg-cover`}
+          />
+          <div className="flex flex-col w-3/4 items-center lg:items-start gap-2 p-0 lg:pl-20">
+            <h1 className="text-pink text-2xl lg:text-4xl font-medium">
+              Guia de pedido de ajuda
+            </h1>
+            <p className="block font-light text-center lg:text-left text-lg lg:text-2xl">
+              Um dos sinais mais conhecidos e amplamente divulgados para
+              mulheres indicarem discretamente que estão em uma situação de
+              violência é o &quot;Sinal de Ajuda&quot;, também conhecido como
+              &quot;Sinal de Violência Doméstica&quot;. Este sinal foi
+              popularizado pela Fundação Canadense de Mulheres e consiste em uma
+              mulher colocar a mão com a palma para cima, dobrar o polegar e, em
+              seguida, fechar os outros dedos sobre o polegar, simulando uma
+              espécie de punho. Este gesto se tornou um código visual
+              internacional para pedir ajuda, especialmente em situações onde a
+              vítima não pode falar abertamente sobre o abuso que está sofrendo.
+              A eficácia deste sinal tem sido comprovada em diversos casos
+              relatados na internet, onde mulheres usaram o gesto em chamadas de
+              vídeo ou mesmo presencialmente para indicar a terceiros que
+              estavam em perigo. É um método discreto e eficaz para comunicar a
+              necessidade de ajuda sem alertar o agressor.
+            </p>
+          </div>
+          <div
+            className="hidden lg:block w-80 h-80
+            bg-[url('../../public/image_close_hand.png')]
+            lg:bg-[url('../../public/image_close_hand.png')] bg-no-repeat bg-center bg-cover"
+          />
+        </div>
       </div>
     </div>
   )
